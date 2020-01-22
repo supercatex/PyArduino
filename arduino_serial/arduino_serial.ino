85667f4b1666cb8acbe93577913fe9ec29e1ca73
Kinda Lam <lamkinun@gmail.com>
2020-01-22 11:33:24 +0800

    init

--- arduino_serial/arduino_serial.ino
+++ arduino_serial/arduino_serial.ino
@@ -1,28 +1,28 @@
-int _length = 0;
-int _pin_states[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int led_status[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void setup() {
-  Serial.begin(9600);
-  while(!Serial) {}
-
-  _length = sizeof(_pin_states) / sizeof(int);
-  
-  for (int i = 2; i < _length; i++) {
+  // put your setup code here, to run once:
+  for (int i = 2; i < 14; i++) {
     pinMode(i, OUTPUT);
   }
+
+  Serial.begin(9600);
+  while (!Serial) {}
 }
 
 void loop() {
-  char buf[_length];
+  // put your main code here, to run repeatedly:
+  char buf[15];
   if (Serial.available() > 0) {
-    int s = Serial.readBytesUntil('\n', buf, _length);
-    for (int i = 0; i < _length; i++) {
-      _pin_states[i] = buf[i] - '0';
+    Serial.readBytesUntil('\n', buf, 15);
+
+    for (int i = 2; i < 14; i++) {
+      led_status[i] = buf[i] - '0';
     }
   }
-  
-  for (int i = 2; i < _length; i++) {
-    digitalWrite(i, _pin_states[i]);
+
+  for (int i = 2; i < 14; i++) {
+    digitalWrite(i, led_status[i]);
   }
   delay(10);
 }
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import time
 import serial
 
 
-pin_states = "0" * 14
-while True:
-    with serial.Serial("COM5", 9600, bytesize=8, parity='N', dsrdtr=True, timeout=10) as ser:
-        print(pin_states)
-        ser.write(bytearray(f"{pin_states}\n", "UTF-8"))
-        if pin_states[2] == "0":
-            pin_states = pin_states[:2] + "1" + pin_states[3:]
-            pin_states = pin_states[:3] + "0" + pin_states[4:]
-        else:
-            pin_states = pin_states[:2] + "0" + pin_states[3:]
-            pin_states = pin_states[:3] + "1" + pin_states[4:]
-
-        time.sleep(1)
+ser = serial.Serial(
+        port="COM5",
+        baudrate=9600,
+        bytesize=8,
+        parity='N',
+        dsrdtr=True,
+        timeout=10
+    )
+ser.write(bytearray("00000000000000\n", "UTF-8"))
+ser.close()
