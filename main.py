68c8e62dbc422964ed0d29a5db871f133f5ae990
Kinda Lam <lamkinun@gmail.com>
2020-02-07 10:29:23 +0800

    sample

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import serial
+import time
 
 
 ser = serial.Serial(
-        port="COM5",
-        baudrate=9600,
-        bytesize=8,
-        parity='N',
-        dsrdtr=True,
-        timeout=10
-    )
-ser.write(bytearray("00000000000000\n", "UTF-8"))
-ser.close()
+    port="COM5",
+    baudrate=9600,
+    bytesize=8,
+    parity='N',
+    dsrdtr=True,
+    timeout=10
+)
+try:
+    while True:
+        ser.write(bytearray("00100000000000\n", "UTF-8"))
+        time.sleep(1)
+        ser.write(bytearray("00010000000000\n", "UTF-8"))
+        time.sleep(1)
+except Exception as e:
+    print(e)
+finally:
+    ser.write(bytearray("00000000000000\n", "UTF-8"))
+    ser.close()
